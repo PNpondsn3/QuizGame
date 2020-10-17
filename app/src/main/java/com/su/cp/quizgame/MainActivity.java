a6909ab2a75e6bd1d488214a2e692400c75551e7
Rattapong Deejaingam <PNdemon@hotmail.com>
2020-10-17 21:17:42 +0700

    Initial commit

---
+++ app/src/main/java/com/su/cp/quizgame/MainActivity.java
@@ -0,0 +1,32 @@
+package com.su.cp.quizgame;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button learnButton = findViewById(R.id.learn_button);
+        Button playButton = findViewById(R.id.play_button);
+        learnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , WordListActivity.class);
+                startActivity(intent);
+            }
+        });
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , GameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
